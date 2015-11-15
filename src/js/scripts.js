f6e03ccdf5969d615323222794fac33020ec9eff
nvenk <nikhilv.13@gmail.com>
2015-11-15 12:06:11 -0800

    Shifting website permanently to git folder
    moved all files, working from this folder now.

---
+++ src/js/scripts.js
@@ -0,0 +1,27 @@
+//-------------------------------------
+// Fade Out Pointer on Scroll
+//-------------------------------------
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('.pointer').fadeOut();
+     }
+    else
+     {
+      $('.pointer').fadeIn();
+     }
+ });
+
+//-------------------------------------
+// Smooth Scroll Pointer
+//-------------------------------------
+
+var $root = $('html, body');
+$('.projects-link').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
