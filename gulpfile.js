0a58827bdb6fa0f0e4f7de0d7120d583524cb170
Nikhil Venkatesh <nikhilv.13@gmail.com>
2016-11-19 22:21:29 -0800

    Conflict resolve

--- gulpfile.js
+++ gulpfile.js
@@ -87,6 +87,10 @@ gulp.task('img', function(){
 	.pipe(newer('assets/img'))
 	.pipe(imagemin({
 		optimizationLevel: 5,
+<<<<<<< Updated upstream
+=======
+      	progressive: true,
+>>>>>>> Stashed changes
       	interlaced: true,
       	svgoPlugins: [{
         	collapseGroups: false,
