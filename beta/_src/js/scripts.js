6e3fab3f466d8f3e3b0ba0009d5f443f5e57ae3a
Nikhil Venkatesh <nikhilv.13@gmail.com>
2017-03-21 16:30:37 -0400

    Beta push 3
    check it out at beta.nvenk.com

--- beta/_src/js/scripts.js
+++ beta/_src/js/scripts.js
@@ -1,32 +1,32 @@
-$('.trigger--about').click(function(e){
-    e.preventDefault();
-    $('body').addClass('transition--about');
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 800);
-    var href = $(this).attr('href');
-    console.log(href);
-    // setTimeout(function() {window.location = href}, 800);
-});
+/* MAKE IT A FUNCTION */
 
-$('.trigger--sim').click(function(e){
-    e.preventDefault();
-    $('body').addClass('transition--sim');
-    $('html, body').animate({
-        scrollTop: $("#sim").offset().top - 78
-    }, 500);
-    var href = $(this).attr('href');
-    console.log(href);
-    setTimeout(function() {window.location = href}, 700);
-});
+var DEBUG = false;
 
-$('.trigger--xtly').click(function(e){
-    e.preventDefault();
-    $('body').addClass('transition--xtly');
-    $('html, body').animate({
-        scrollTop: $("#xtly").offset().top - 78
-    }, 500);
-    var href = $(this).attr('href');
-    console.log(href);
-    setTimeout(function() {window.location = href}, 700);
-});
+
+function trigger(id, scrollOffset){
+    var btn = '.trigger--' + id;
+
+    if(DEBUG == true) { console.log("iD=" + id,"BTN=" + btn); };
+
+    $(btn).click(function(e){
+        e.preventDefault();
+
+        $('body').addClass('transition--' + id);
+        $('html, body').animate({
+            scrollTop: $("#" + id).offset().top - scrollOffset
+        }, 600);
+
+        var href = $(btn).attr('href');
+
+        if(DEBUG == true) { console.log(href); };
+
+        setTimeout(function() {window.location = href}, 800);
+    });
+};
+
+
+tList = ['about','sim','xtly','central','puppy'];
+
+for (i=0; i<tList.length; i++){
+    trigger(tList[i], 78);
+}
