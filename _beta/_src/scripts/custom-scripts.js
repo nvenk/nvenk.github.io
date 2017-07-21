df7303705485c388a663a058af1dd2ffa39581d7
Nikhil Venkatesh <nikhilv.13@gmail.com>
2017-07-21 15:45:33 -0700

    Beta updates

--- _beta/_src/scripts/custom-scripts.js
+++ _beta/_src/scripts/custom-scripts.js
@@ -1,32 +1,6 @@
 var DEBUG = false;
 
 $(window).on('load',function() {
-    var primeCtrl = new ScrollMagic.Controller({ name: 'primeTrigger' });
-    var leaveCtrl = new ScrollMagic.Controller({ name: 'leaveTrigger',
-                                                 globalSceneOptions: { triggerHook: 'onLeave' }});
-
-    // Hero Image
-    var fadeTween = TweenMax.fromTo('.hero',
-                                    1,
-                                    { opacity: 0.6 },
-                                    { opacity: 0 });
-    var heroScene = new ScrollMagic.Scene({ duration: 350 })
-                                            .setTween(fadeTween)
-                                            .addTo(primeCtrl);
-    if(DEBUG) heroScene.addIndicators({ name:"Fade Hero" });
-
-    // Skill Icons Stagger
-    var skillTween = TweenMax.staggerFromTo(".skill-icons .icon",
-                                            0.5,
-                                            { opacity: 0, y: -60 },
-                                            { opacity: 1, y: 0 },
-                                            0.15 );
-    var skillScene = new ScrollMagic.Scene({ triggerElement: '.skill-icons',
-                                            //  reverse: false,
-                                             duration: 150 })
-                                             .setTween(skillTween)
-                                             .addTo(primeCtrl);
-    if(DEBUG) skillScene.addIndicators({ name:"Fade Icons" });
 
     // Setting Trigger Animations
 
@@ -37,7 +11,7 @@ $(window).on('load',function() {
     $(expTrigger).click(function(e) {
         e.preventDefault();
 
-        $(this).parent('section').addClass('active');
+        $(this).closest('section').addClass('active');
         $('section').not($('.active')).addClass('hide');
 
         $('html, body').animate({
@@ -45,14 +19,19 @@ $(window).on('load',function() {
         }, expScrollSpeed);
 
         var href = $(this).attr('href');
+        var timeout = 620;
 
         if (href === undefined) href = $(this).attr('data-href');
 
         if(DEBUG) console.log(href);
 
+        if(href == 'about.html') {
+            timeout = 1000;
+        }
+
         setTimeout(function() {
             window.location = href;
-        }, 620);
+        }, timeout);
     });
 
 
